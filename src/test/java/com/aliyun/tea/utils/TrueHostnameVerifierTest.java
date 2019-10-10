6f4488bce81b57c1573862423b5ae230dab33f6a
你çwb-czy543427 <wb-czy543427@alibaba-inc.com>
2019-10-10 10:33:22 +0800

    add utils test code

---
+++ src/test/java/com/aliyun/tea/utils/TrueHostnameVerifierTest.java
@@ -0,0 +1,17 @@
+package com.aliyun.tea.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.net.ssl.SSLSession;
+
+public class TrueHostnameVerifierTest {
+    @Test
+    public void TrueHostnameVerifierTest(){
+        TrueHostnameVerifier trueHostnameVerifier = new TrueHostnameVerifier();
+        SSLSession sslSession = null;
+        trueHostnameVerifier.verify("authType",sslSession);
+        Assert.assertTrue(trueHostnameVerifier.verify(null,null));
+
+    }
+}
