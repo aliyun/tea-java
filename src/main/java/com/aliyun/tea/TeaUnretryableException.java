345ea80bff53028e370cdc62f491dcab0118bad8
Jackson Tian <shyvo1987@gmail.com>
2019-10-13 00:51:48 +0800

    Add missed Exceptions

---
+++ src/main/java/com/aliyun/tea/TeaUnretryableException.java
@@ -0,0 +1,13 @@
+package com.aliyun.tea;
+
+public class TeaUnretryableException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -7006694712718176751L;
+
+    public TeaUnretryableException(TeaRequest lastRequest) {
+        // TODO
+    }
+}
