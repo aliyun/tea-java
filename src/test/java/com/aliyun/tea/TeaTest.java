0a28e3aeb95ebe6f925c4b6fbd907b2eab232415
你çwb-czy543427 <wb-czy543427@alibaba-inc.com>
2019-10-23 10:21:37 +0800

    perfect teaTest

--- src/test/java/com/aliyun/tea/TeaTest.java
+++ src/test/java/com/aliyun/tea/TeaTest.java
@@ -1,5 +1,6 @@
 package com.aliyun.tea;
 
+import com.aliyun.tea.utils.StringUtils;
 import com.aliyun.tea.utils.X509TrustManagerImp;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,11 +16,13 @@ import java.util.Map;
 
 import static org.mockito.Mockito.mock;
 
+
 public class TeaTest {
     @Test
     public void init() {
         new Tea();
     }
+
 
     @Test
     public void composeUrlTest() throws UnsupportedEncodingException, NoSuchMethodException, InvocationTargetException,
@@ -28,9 +31,40 @@ public class TeaTest {
         composeUrl.setAccessible(true);
 
         TeaRequest request = new TeaRequest();
+        Map<String,String> headers = new HashMap<>();
+        headers.put("test","test");
         request.protocol = "HTTP";
+        request.pathname = "?";
+        request.method = "GET";
+        request.headers = headers;
+        request.query = headers;
+
         String str = (String) composeUrl.invoke(Tea.class, request);
-        Assert.assertEquals("HTTP://nullnull", str);
+        Assert.assertEquals("HTTP://null??test=test", str);
+
+    }
+
+    @Test
+    public void doActionTest() throws Exception{
+        Map<String,Object> map = new HashMap<>();
+        TeaRequest request = new TeaRequest();
+        try {
+            Tea.doAction(request,map);
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertEquals("nullnull", e.getMessage());
+        }
+        map.put("readTimeout", 1000);
+        map.put("connectTimeout", 1000);
+        request = new TeaRequest();
+        request.protocol = "https";
+        Map<String, String> header = new HashMap<>();
+        header.put("host", "www.baidu.com");
+        request.headers = header;
+        request.pathname = "";
+        request.body = "{}";
+        request.method = "PUT";
+        Assert.assertNotNull(Tea.doAction(request,map));
     }
 
     @Test
@@ -50,26 +84,30 @@ public class TeaTest {
         SSLSocketFactory sslSocketFactory = Whitebox.invokeMethod(tea, "createSSLSocketFactory");
         Assert.assertNotNull(sslSocketFactory);
     }
+    @Test
+    public void allowRetryTest(){
+        long now = 1L;
+        Map<String,Object> map = new HashMap<>();
+        Assert.assertEquals(false,Tea.allowRetry(map,1,now));
+        Assert.assertEquals(true,Tea.allowRetry(map,0,now));
+    }
 
     @Test
-    public void getBackoffTimeTest() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("policy", "");
-        int number = Tea.getBackoffTime(map, 88);
-        Assert.assertEquals(0, number);
-
-        map.put("policy", "no");
-        number = Tea.getBackoffTime(map, 88);
-        Assert.assertEquals(0, number);
-
-        map.put("policy", "one");
-        map.put("period", 0);
-        number = Tea.getBackoffTime(map, 88);
-        Assert.assertEquals(88, number);
-
-        map.put("policy", "one");
-        map.put("period", 66);
-        number = Tea.getBackoffTime(map, 88);
-        Assert.assertEquals(66, number);
+    public void getBackoffTimeTest()  {
+        StringUtils stringUtils = new StringUtils();
+        Map<String,Object> map = new HashMap<>();
+        stringUtils.isEmpty("policy");
+        Assert.assertEquals(0,Tea.getBackoffTime(map,0));
+    }
+    @Test
+    public void sleepTest() throws InterruptedException {
+        int time = 0;
+        Tea.sleep(time);
+        Assert.assertNotNull(time);
+    }
+    @Test
+    public void isRetryableTest() {
+        Exception exception = new Exception();
+        Assert.assertEquals(false,Tea.isRetryable(exception));
     }
 }
